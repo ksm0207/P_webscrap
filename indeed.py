30fd05d1aad2577daec7264b5b863c6c3f67aea7
ksm0207 <ksm03071@naver.com>
2020-11-12 16:35:01 +0900

    페이지 개수 만큼 요청하기 완료
    - indeed.py 파일생성
    - 기존코드 함수처리
    - 최대 페이지 개수 만큼 요청 확인완료

---
+++ indeed.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup  # 데이터를 추출하기 위해 import
+
+
+LIMIT = 50
+URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+
+def extract_indeed_pages():
+    # indeed html을 가져옵니다
+    indeed = requests.get(URL)
+
+    # 데이터를 추출할 변수를 만들어 줍니다
+    indeed_soup = BeautifulSoup(indeed.text, "html.parser")
+
+    # div 요소에 class 명이 pagination 으로 작성 된 것을 찾습니다
+    pagination = indeed_soup.find("div", {"class": "pagination"})
+
+    # find_all() 으로 모든 <a> 요소를 찾은다음 리스트[]로 반환합니다
+    links = pagination.find_all("a")
+
+    pages = []
+    # for 반복문 시작
+    for link in links[:-3]:
+        pages.append(int(link.string))
+
+    # 페이지에서 가장 큰 숫자를 나타냅니다
+    max_page = pages[-1]
+
+    # 페이지에서 가장 큰 숫자를 반환합니다
+    return max_page
+
+
+def extract_indeed_jobs(last_pages):
+    jobs = []
+    for page in range(last_pages):
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        print(result.status_code)
+    return jobs
+
+
+# jobs = []
+# for page in range(last_pages):
+# result = requests.get(f"{URL}&start={page*LIMIT}")
+# print("extract_indeed_jobs() = ", result.status_code)
+# return jobs
+
--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-import requests
-from bs4 import BeautifulSoup  # 데이터를 추출하기 위해 import
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
+# 페이지의 가장 큰 숫자는 18을 나타냅니다
+max_indeed_pages = extract_indeed_pages()
 
-# indeed html을 가져옵니다
-indeed = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
-
-# 데이터를 추출할 변수를 만들어 줍니다
-indeed_soup = BeautifulSoup(indeed.text, "html.parser")
-
-# div 요소에 class 명이 pagination 으로 작성 된 것을 찾습니다
-pagination = indeed_soup.find("div", {"class": "pagination"})
-
-
-# find_all() 으로 모든 <a> 요소를 찾은다음 리스트[]로 반환합니다
-links = pagination.find_all("a")
-
-pages = []
-# for 반복문 시작
-for link in links[:-1]:
-    pages.append(int(link.string))
-
-
-max_page = pages[-1]
-
-print(max_page)
+# 페이지의 가장 큰 숫자를 받아 18번동안 작동합니다
+indeed_jobs = extract_indeed_jobs(max_indeed_pages)
