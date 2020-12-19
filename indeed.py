ecbe4acb44a7c12b66cb7213b15b582d088172d2
ksm0207 <ksm03071@naver.com>
2020-12-19 17:42:31 +0900

    장소 & 페이지링크 가져오기 완료

--- indeed.py
+++ indeed.py
@@ -31,23 +31,38 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_job(html):  # html = extract_indeed_jobs() result 인자를 받고 정보를 추출합니다
+    # html 인자를 받고 extract_indeed_jobs 의 request 한 결과를 출력합니다 
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})     # 회사이름 가져오기
+    company_anchor = company.find("a")  # Company 에서 soup 으로 부터 찾은 결과 저장
+    # 회사 이름에 <a> 없는곳이 있는지 체크해주기
+    if company_anchor is not None:
+        # 회사에 링크가 있다면 <a> String 출력
+        company = str(company_anchor.string)  # strip() 공백제거
+    else:
+        # 링크가 없으면 아래 것 을 출력
+        company = str(company.string)
+    company = company.strip()
+    
+    # 장소 가져오기
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]  # [data-rc-loc] 은 div 안에 있는 attribute 에 접근합니다
+    # 페이지 지원링크 (id 값) 가져오기
+    job_id = html["data-jk"]
+    return {'title': title, 'company': company, 'location': location, 'link': f"https://www.indeed.com/viewjob?jk{job_id}"}
+
+
 def extract_indeed_jobs(last_pages):
-    jobs = []
-    # for page in range(last_pages):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-    for job_result in results:
-        title = job_result.find("h2", {"class": "title"}).find("a")["title"]
-        company = job_result.find("span", {"class": "company"})     # 회사이름 가져오기
-        company_anchor = company.find("a")  # Company 에서 soup 으로 부터 찾은 결과 저장
-        # 회사 이름에 <a> 없는곳이 있는지 체크해주기
-        if company_anchor is not None:
-            # 회사에 링크가 있다면 <a> String 출력
-            company = company_anchor.string.strip()  # strip() 공백제거
-            print(title, company)
-        else:
-            # 링크가 없으면 아래 것 을 출력
-            company = company.string.strip()
+    jobs = []   # extract_job() 함수로 부터 반환된 값을 배열에 넣습니다
+    for page in range(last_pages):  # last_page = 18
+        print(f"스크랩핑 페이지 수 : {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            # extract_job() 함수 호출 후 결과를 출력합니다
+            job = extract_job(result)
+            # extract_job -->  company or title 결과를 배열로 저장합니다
+            jobs.append(job)
     return jobs
 
--- main.py
+++ main.py
@@ -5,3 +5,5 @@ max_indeed_pages = extract_indeed_pages()
 
 # 페이지의 가장 큰 숫자를 받아 18번동안 작동합니다
 indeed_jobs = extract_indeed_jobs(max_indeed_pages)
+
+print(indeed_jobs)
