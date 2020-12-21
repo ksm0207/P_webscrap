afd662dfdb54b7bb263cce575ca7936ae3ab0f15
ksm0207 <ksm03071@naver.com>
2020-12-21 15:27:24 +0900

    페이지 엑셀 저장 확인완료

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from save import save_to_file
 
 stack_over = stack_get_jobs()
 indeed_jobs = get_jobs()
-
 jobs = stack_over + indeed_jobs     # 엑셀 시트에 넣을 변수 2개의 Array를 합침
 
 # jobs를 파일로 저장함 --> 구글시트
