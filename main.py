679dbe93205ec564a1f2a0f76e9c01f53a23a70d
ksm0207 <ksm03071@naver.com>
2020-12-21 11:42:55 +0900

    StackOverflow 마무리
    - 회사 로케이션 링크 작업 완료

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# from indeed import get_jobs
-
+from indeed import get_jobs
 from stackoverflow import stack_get_jobs
 
 
-# indeed_jobs = get_jobs()
-
 stack_over = stack_get_jobs()
+indeed_jobs = get_jobs()
 
+jobs = stack_over + indeed_jobs     # 엑셀 시트에 넣을 변수 2개의 Array를 합침
 
+print(jobs)
--- stackoverflow.py
+++ stackoverflow.py
@@ -24,30 +24,31 @@ def get_last_page():
 # 일자리 가져오기
 def extract_job(html):  # html 인자는 result 를 받음
     title = html.find("div", {"class": "grid--cell fl1"}).find("h2").find("a")["title"]  # 공백주의!
-    
     # 리스트에 요소가 2개인지 이미 알고 있을때 변수 따로 처리하기
     # 첫번째요소 : company
     # 두번째요소 : location
     company, location = html.find("h3", {
         "class": "fc-black-700 fs-body1 mb4"
         }).find_all("span", recursive=False)  # recursive = 전부 가져오는것을 방지함
-
     company = company.get_text(strip=True)
     location = location.get_text(strip=True).strip("-")
-    return {'title': title, 'company': company, 'location': location}
+    # job id 가져오기 (URL)
+    job_id = html["data-jobid"]
+
+    return {'title': title, 'company': company, 'location': location, 'link': f"https://stackoverflow.com/jobs/{job_id}"}
 
 
 # job 출력하기 indeed.py extract_indeed_jobs() 참고
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"SOF : 스크랩핑 페이지 수 : {page}")
         result = requests.get(f"{URL}&pg={page +1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
             job = extract_job(result)
             jobs.append(job)
-            print(jobs)
     return jobs
 
 
