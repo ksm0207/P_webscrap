df33e57afb28901035fdab1b5d858d4735eff6ff
ksm0207 <ksm03071@naver.com>
2020-12-22 11:30:01 +0900

    Flask 학습

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("SuperScrapper")  # 앱 이름 설정 / 는 root(웹사이트)를 의미함 google.com/ 구글루트 접속
 
 
-@app.route("/")
-def hello():
-    return "Helloasdsad"
+@app.route("/")  # / 으로 접속했을떄 home 이라는 함수를 실행함
+def home():
+    return "Hello"
 
 
-if __name__ == "__main__":
-    app.run(debug=1)
+# Python은 @를 찾아 어떤 접속요청이 들어오면 실행
+# @ : 데코레이터 라고 부르며 아래에 있는 함수를 찾아 실행함
+@app.route("/about")  # /about 접속시 함수 실행
+def about_me():
+    return "About"
+
+
+app.run()
